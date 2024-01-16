e89ecb3f69b5d8be7439f53f8228380a61b9c62a
GatereKinyanjui <gaterekinyanjui@gmail.com>
2024-01-16 14:48:43 +0300

    initial use-state-practice commit

---
+++ src/components/UseState/index.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import "./UseState.css";
+
+const UseState = () => {
+  const [theme, setTheme] = useState("dark");
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className={`state ${theme}`}>
+      <h1>UseState Component</h1>
+      <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+        Toggle theme
+      </button>
+
+      <h2>DISPLAY COUNT HERE</h2>
+      <h1 className="count-display">{count}</h1>
+      <button onClick={() => setCount((prevCount: number) => prevCount + 1)}>
+        Increment
+      </button>
+      <button onClick={() => setCount((prevCount: number) => prevCount - 1)}>
+        Decrement
+      </button>
+    </div>
+  );
+};
+
+export default UseState;
